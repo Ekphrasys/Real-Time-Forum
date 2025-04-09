0c7f0b7a36ee551bf801450d1aa7d6a25594eb1e
scointin <samson.cointin@gmail.com>
2025-04-09 11:26:57 +0200

    oupsi

--- server/authHandler.go
+++ server/authHandler.go
@@ -26,23 +26,3 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintln(w, "User successfully registered")
 }
-
-// func registerHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.Method != http.MethodPost {
-// 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-// 		return
-// 	}
-
-// 	// Lire et traiter le corps de la requête
-// 	var requestData map[string]interface{}
-// 	err := json.NewDecoder(r.Body).Decode(&requestData)
-// 	if err != nil {
-// 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// Réponse de confirmation
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
-// }
